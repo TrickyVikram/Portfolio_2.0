0b01936cfe02bc9fda483cb29cbdebc22efd9951
TrickyVikram <vkumarsah999@gmail.com>
2024-08-26 04:03:33 +0530

    update9

--- src/data/constants.js
+++ src/data/constants.js
@@ -249,7 +249,7 @@ export const experiences = [
     img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Rudraksha.jpeg?alt=media&token=8f83f41e-d0a1-486f-9c7f-dd4cd1d09e8d",
     role: "Android Developer Intern",
     company: "Rudraksha Welffare Foundation",
-    date: "June 2021 - Oct 2021",
+    date: "June 2022 - Oct 2022",
     desc: "As an Android Developer Intern at Rudraksha Welfare Foundation from June 2021 to October 2021, I gained valuable hands-on experience in application development. During my tenure, I contributed to the development of the application's back-end cloud functions using Firebase and implemented a front-end with the MVVM structure, which was designed using Figma. I also added a one-to-one video call feature, conceptualized new features, and optimized app performance. This internship helped me enhance my skills in Android development and gave me an opportunity to work in a collaborative team environment.",
     skills: [
       "Android",
